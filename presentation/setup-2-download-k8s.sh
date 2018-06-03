369227d33dca8a12167b0bf37948b7ee62e22d6a
Joshua Sheppard <Joshua.Sheppard@infinitecampus.com>
2018-06-03 15:43:25 -0500

    Improve `wget` urls during setup

--- presentation/setup-2-download-k8s.sh
+++ presentation/setup-2-download-k8s.sh
@@ -6,29 +6,36 @@ https://storage.googleapis.com/kubernetes-release/release/v1.9.7/bin/linux/amd64
 chmod +x kubelet
 
 # Get kube-apiserver
-wget https://storage.googleapis.com/kubernetes-release/release/v1.9.7/bin/linux/amd64/kube-apiserver
+wget -q --show-progress --https-only --timestamping \
+https://storage.googleapis.com/kubernetes-release/release/v1.9.7/bin/linux/amd64/kube-apiserver
 chmod +x kube-apiserver
 
 # Get kubectl
-wget https://storage.googleapis.com/kubernetes-release/release/v1.9.7/bin/linux/amd64/kubectl
+wget -q --show-progress --https-only --timestamping \
+https://storage.googleapis.com/kubernetes-release/release/v1.9.7/bin/linux/amd64/kubectl
 chmod +x kubectl
 
 # Get kube-scheduler
-wget https://storage.googleapis.com/kubernetes-release/release/v1.9.7/bin/linux/amd64/kube-scheduler
+wget -q --show-progress --https-only --timestamping \
+https://storage.googleapis.com/kubernetes-release/release/v1.9.7/bin/linux/amd64/kube-scheduler
 chmod +x kube-scheduler
 
 # Get kube-controller-manager
-wget https://storage.googleapis.com/kubernetes-release/release/v1.9.7/bin/linux/amd64/kube-controller-manager
+wget -q --show-progress --https-only --timestamping \
+https://storage.googleapis.com/kubernetes-release/release/v1.9.7/bin/linux/amd64/kube-controller-manager
 chmod +x kube-controller-manager
 
 # Get kubeconfig
-wget https://raw.githubusercontent.com/joshuasheppard/k8s-by-component/master/part3/kubeconfig
+wget -q --show-progress --https-only --timestamping \
+https://raw.githubusercontent.com/joshuasheppard/k8s-by-component/master/part3/kubeconfig
 
 # Get pod definition
-wget https://raw.githubusercontent.com/joshuasheppard/k8s-by-component/master/part3/nginx.yaml
+wget -q --show-progress --https-only --timestamping \
+https://raw.githubusercontent.com/joshuasheppard/k8s-by-component/master/part3/nginx.yaml
 
 # Get deployment definition
-wget https://raw.githubusercontent.com/joshuasheppard/k8s-by-component/master/presentation/nginx-deployment.yaml
+wget -q --show-progress --https-only --timestamping \
+https://raw.githubusercontent.com/joshuasheppard/k8s-by-component/master/presentation/nginx-deployment.yaml
 
 # Create manifests directory for bare kubelet example
 mkdir manifests
