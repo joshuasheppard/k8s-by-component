cd7fc8b9f8d45c0f7d4e52cdf6fec0e741ad5b4c
Joshua Sheppard <josh@sheppardcentral.net>
2019-09-29 10:06:24 -0500

    Updated Kubernetes version to 1.15.4

--- presentation/k8s-1.15/setup-2-download-k8s-1.15.sh
+++ presentation/k8s-1.15/setup-2-download-k8s-1.15.sh
@@ -3,11 +3,11 @@ cd ~/
 
 # Download the Kubernetes components
 wget -q --show-progress --https-only --timestamping \
-https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kubelet \
-https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-apiserver \
-https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kubectl \
-https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-scheduler \
-https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-controller-manager
+https://storage.googleapis.com/kubernetes-release/release/v1.15.4/bin/linux/amd64/kubelet \
+https://storage.googleapis.com/kubernetes-release/release/v1.15.4/bin/linux/amd64/kube-apiserver \
+https://storage.googleapis.com/kubernetes-release/release/v1.15.4/bin/linux/amd64/kubectl \
+https://storage.googleapis.com/kubernetes-release/release/v1.15.4/bin/linux/amd64/kube-scheduler \
+https://storage.googleapis.com/kubernetes-release/release/v1.15.4/bin/linux/amd64/kube-controller-manager
 chmod +x kubelet kube-apiserver kubectl kube-scheduler kube-controller-manager
 
 # Get kubeconfig
